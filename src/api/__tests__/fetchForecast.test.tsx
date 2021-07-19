a7a5005c1748443dfd88dce605be39191a7a0797
Marcelo <marcelodpaternostro@gmail.com>
2021-07-18 21:25:46 -0300

    feat: add fetchForecast tests

--- src/api/__tests__/fetchForecast.test.tsx
+++ src/api/__tests__/fetchForecast.test.tsx
@@ -1,10 +1,50 @@
+import { rest } from "msw";
+import { server } from "../../mocks/server";
 import { fetchForecast } from "../fetchForecast";
-import returnForecastData from "../../fixtures/return-forecast-data";
 
 describe("fetchForecast", () => {
   test("should return forecast data", async () => {
     const result = await fetchForecast();
+    expect(result).toHaveProperty("city");
+    expect(result).toHaveProperty("list");
+    expect(result.list[0].date).toBeInstanceOf(Date);
+  });
 
-    expect(result).toEqual(returnForecastData);
+  test("should throw an error if apiKey is not provided", async () => {
+    process.env.SNOWPACK_PUBLIC_OPEN_WEATHER_API_KEY = "";
+    await expect(fetchForecast()).rejects.toThrow(
+      /^OPEN_WEATHER_API_KEY node env not provided.$/
+    );
+    process.env.SNOWPACK_PUBLIC_OPEN_WEATHER_API_KEY = "app-test-id";
+  });
+
+  test("should throw an error if api response returned an error", async () => {
+    server.use(
+      rest.get(
+        "https://api.openweathermap.org/data/2.5/forecast",
+        (req, res, ctx) => {
+          return res(
+            ctx.status(500),
+            ctx.json({ cod: 500, message: "API currently under maintenance." })
+          );
+        }
+      )
+    );
+
+    await expect(fetchForecast()).rejects.toThrow(
+      /^Failed while loading data. Please try again later.$/
+    );
+  });
+
+  test("should throw an error if fetch promise failed", async () => {
+    server.use(
+      rest.get("https://api.openweathermap.org/data/2.5/forecast", () => {
+        throw new Error("Failed to fetch.");
+      })
+    );
+
+    await expect(fetchForecast()).rejects.toThrow(
+      /^No internet connection available. Please try again later.$/
+    );
   });
 });
--- src/api/fetchForecast.tsx
+++ src/api/fetchForecast.tsx
@@ -3,12 +3,22 @@ import { mapForecast } from "../mappers/forecast";
 
 export async function fetchForecast(): Promise<ForecastData> {
   const apiKey = process.env.SNOWPACK_PUBLIC_OPEN_WEATHER_API_KEY;
-  if (typeof apiKey !== "string") {
+  if (!apiKey || typeof apiKey !== "string") {
     throw new Error("OPEN_WEATHER_API_KEY node env not provided.");
   }
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?id=3435910&units=metric&appid=${apiKey}`
-  );
+  let response;
+  try {
+    response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?id=3435910&units=metric&appid=${apiKey}`
+    );
+  } catch {
+    throw new Error(
+      "No internet connection available. Please try again later."
+    );
+  }
+  if (!response.ok) {
+    throw new Error("Failed while loading data. Please try again later.");
+  }
   const data = (await response.json()) as FiveDayForecastRawData;
   return {
     city: data.city,
